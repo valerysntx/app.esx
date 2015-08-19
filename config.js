896cfade700281dc6f8c33642c0830797e947b38
valery.sntx <valery.sntx@gmail.com>
2015-08-19 07:55:33 +0300

    ci with appveyor, using jspm build

--- config.js
+++ config.js
@@ -1,48 +1,33 @@
-var debug = {
-
-  "baseURL": "/",
-  "transpiler": "traceur",
-  "paths": {
-     "*": "*.js",
-     "github:*": "jspm_packages/github/*.js",
-     "npm:*":"jspm_packages/npm/*.js"
-  },
- "map": {
-  "md":"github:guybedford/system-md@0.0.1",
-  "showdown@0.3.1": "jspm_packages/github/showdownjs/showdown@0.3.4/compressed/showdown.min",
-    "guybedford/system-md": "github:guybedford/system-md@0.0.1",
-    "knockout": "github:knockout/knockout@3.3.0",
-    "traceur": "node_modules/traceur/bin/traceur",
-    "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.88",
-    "github:guybedford/system-md@0.0.1": {
-      "showdown@0.3.1": "github:showdownjs/showdown@0.3.4"
-    }
-  }
-  
-};
-
-var github = {
-
+System.config({
   "baseURL": "/",
   "transpiler": "traceur",
   "paths": {
     "*": "*.js",
-    //'*': 'https://registry.jspm.io/*.js',
-    "github:*" : 'https://github.jspm.io/*.js', 
-    "npm:*":"jspm_packages/npm/*.js"
+    "github:*": "jspm_packages/github/*.js",
+    "npm:*": "jspm_packages/npm/*.js"
   },
- "map": {
-  "md":"github:guybedford/system-md@0.0.1",
-  "showdown@0.3.1": "jspm_packages/github/showdownjs/showdown@0.3.4/compressed/showdown.min",
+  "bundles": {
+    "dist/app": [
+      "github:knockout/knockout@3.3.0/dist/knockout.debug",
+      "header",
+      "jumbo",
+      "github:knockout/knockout@3.3.0",
+      "ApplicationViewModel"
+    ]
+  }
+});
+
+System.config({
+  "map": {
     "guybedford/system-md": "github:guybedford/system-md@0.0.1",
     "knockout": "github:knockout/knockout@3.3.0",
-    "traceur": "node_modules/traceur/bin/traceur",
+    "md": "github:guybedford/system-md@0.0.1",
+    "showdown@0.3.1": "jspm_packages/github/showdownjs/showdown@0.3.4/compressed/showdown.min",
+    "traceur": "github:jmcriffey/bower-traceur@0.0.88",
     "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.88",
     "github:guybedford/system-md@0.0.1": {
       "showdown@0.3.1": "github:showdownjs/showdown@0.3.4"
     }
   }
-  
-};
+});
 
-System.config(debug);
